5e0d9ffcb8907a9ca16c21115cdc167ba96eff1c
LIlia <lilia_dyrnina@mail.ru>
2024-09-26 17:31:33 +0300

    h/w2_3

---
+++ 2week/str8_leetcode.py
@@ -0,0 +1,25 @@
+"""https://leetcode.com/problems/count-and-say/description/?envType=problem-list-v2&envId=string&difficulty=MEDIUM%2CHARD"""
+
+
+def countAndSay(n: int) -> str:
+    s = "1"
+    curr_s = ""
+
+    for i in range(1, n):
+        x = 0
+        counter = 0
+        for y in range(0, len(s)):
+            if s[x] == s[y]:
+
+                counter += 1
+            else:
+                curr_s += str(counter) + s[x]
+                counter = 1
+                x = y
+        s = curr_s + str(counter) + s[x]
+        curr_s = ""
+        print(s)
+    return s
+
+
+countAndSay(10)
