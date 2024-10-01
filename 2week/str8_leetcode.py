554b7ba845065ba81cbe0e16e1a573c0bc577e40
LIlia <lilia_dyrnina@mail.ru>
2024-10-01 22:20:05 +0300

    3week_1

--- 2week/str8_leetcode.py
+++ 2week/str8_leetcode.py
@@ -1,25 +1,21 @@
 """https://leetcode.com/problems/count-and-say/description/?envType=problem-list-v2&envId=string&difficulty=MEDIUM%2CHARD"""
 
 
-def countAndSay(n: int) -> str:
-    s = "1"
-    curr_s = ""
-
-    for i in range(1, n):
-        x = 0
-        counter = 0
-        for y in range(0, len(s)):
-            if s[x] == s[y]:
-
-                counter += 1
-            else:
-                curr_s += str(counter) + s[x]
-                counter = 1
-                x = y
-        s = curr_s + str(counter) + s[x]
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s = "1"
         curr_s = ""
-        print(s)
-    return s
+        for i in range(1, n):
+            x = 0
+            counter = 0
+            for y in range(0, len(s)):
+                if s[x] == s[y]:
 
-
-countAndSay(10)
+                    counter += 1
+                else:
+                    curr_s += str(counter) + s[x]
+                    counter = 1
+                    x = y
+            s = curr_s + str(counter) + s[x]
+            curr_s = ""
+        return s
