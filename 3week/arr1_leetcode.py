dfd1695bc68cd8a2a1b3284f48ebe924ebb2a476
LIlia <lilia_dyrnina@mail.ru>
2024-10-02 23:02:11 +0300

    3week

--- 3week/arr1_leetcode.py
+++ 3week/arr1_leetcode.py
@@ -1,16 +1,14 @@
 """ https://leetcode.com/problems/first-missing-positive/description/?envType=problem-list-v2&envId=array"""
 
 
-def firstMissingPositive(nums: list[int]) -> int:
-    nums = sorted([x for x in nums if x >= 1])
-    print(nums)
-    if len(nums) == 0 or nums[0] != 1:
-        return 1
+class Solution:
+    def firstMissingPositive(self, nums: list[int]) -> int:
+        nums = sorted([x for x in set(nums) if x >= 1])
+        print(nums)
+        if len(nums) == 0 or nums[0] != 1:
+            return 1
 
-    for i in range(0, len(nums) - 1):
-        if nums[i + 1] - nums[i] > 1:
-            return nums[i] + 1
-    return nums[-1] + 1
-
-
-print(firstMissingPositive([0, 2, 2, 1, 1]))
+        for i in range(0, len(set(nums)) - 1):
+            if nums[i + 1] - nums[i] > 1:
+                return nums[i] + 1
+        return nums[-1] + 1
