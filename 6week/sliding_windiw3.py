774a807967223e56713d95267e1d9665cccd8087
LIlia <lilia_dyrnina@mail.ru>
2024-11-14 18:48:42 +0300

    6week3

---
+++ 6week/sliding_windiw3.py
@@ -0,0 +1,22 @@
+"""https://leetcode.com/problems/find-all-anagrams-in-a-string/description/?envType=problem-list-v2&envId=sliding-window"""
+
+
+def findAnagrams(s: str, p: str) -> list[int]:
+    if len(p) > len(s):
+        return []
+    str = ""
+    listic = []
+
+    p = sorted(p)
+    print(p)
+
+    for i in range(0, len(s) - len(p) + 1):
+
+        str = sorted(s[i : i + len(p)])
+
+        if str == p:
+            listic.append(i)
+    return listic
+
+
+print(findAnagrams("cbaebabacd", "abc"))
