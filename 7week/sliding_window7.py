a0be35ad90203d628850ebd90ea1863ec28e8468
LIlia <lilia_dyrnina@mail.ru>
2024-11-22 19:33:28 +0300

    7week_2

---
+++ 7week/sliding_window7.py
@@ -0,0 +1,18 @@
+"""https://leetcode.com/problems/find-the-k-beauty-of-a-number/?envType=problem-list-v2&envId=sliding-window"""
+
+
+class Solution:
+    def divisorSubstrings(self, num: int, k: int) -> int:
+
+        response = 0
+
+        for i in range(len(str(num)) - k + 1):
+            stroka = str(num)[i : i + k]
+            if int(stroka) != 0:
+                if num % int(stroka) == 0:
+                    response += 1
+
+        return response
+
+
+# print(divisorSubstrings(2, 1))
