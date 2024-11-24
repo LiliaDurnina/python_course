5fb5deb61cef30d1bd1aba4a5bb7e1909eeea34a
LIlia <lilia_dyrnina@mail.ru>
2024-11-24 16:23:11 +0300

    7week_3-4

---
+++ 7week/sliding_windiw8.py
@@ -0,0 +1,30 @@
+"""https://leetcode.com/problems/count-the-number-of-good-subarrays/?envType=problem-list-v2&envId=sliding-window"""
+
+
+class Solution:
+    def countGood(self, nums: list[int], k: int) -> int:
+
+        n = len(nums)
+        left = 0
+        count = 0
+        freq = {}
+        cur = 0
+        for right in range(n):
+            if nums[right] in freq:
+                freq[nums[right]] += 1
+            else:
+                freq[nums[right]] = 1
+            cur += freq[nums[right]] - 1
+            while cur >= k:
+                count += n - right
+                freq[nums[left]] -= 1
+
+                if freq[nums[left]] == 0:
+                    del freq[nums[left]]
+                cur -= freq.get(nums[left], 0)
+                left += 1
+
+        return count
+
+
+# print(count_good_subarrays([3, 1, 4, 3, 2, 2, 4], 2))  # Вывод: 4
---
+++ 7week/sliding_window9.py
@@ -0,0 +1,45 @@
+"""https://leetcode.com/problems/minimum-window-substring/?envType=problem-list-v2&envId=sliding-window"""
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not t or not s:
+            return ""
+
+        dict_t = {}
+        for char in t:
+            dict_t[char] = dict_t.get(char, 0) + 1
+
+        length = len(dict_t)
+
+        l, r = 0, 0
+        window_counts = {}
+
+        min_len = len(s) + 1
+        min_left, min_right = 0, 0
+
+        f = 0
+
+        while r < len(s):
+            char = s[r]
+            window_counts[char] = window_counts.get(char, 0) + 1
+
+            if char in dict_t and window_counts[char] == dict_t[char]:
+                f += 1
+
+            while l <= r and f == length:
+                char = s[l]
+
+                if r - l + 1 < min_len:
+                    min_len = r - l + 1
+                    min_left, min_right = l, r + 1
+
+                window_counts[char] -= 1
+                if char in dict_t and window_counts[char] < dict_t[char]:
+                    f -= 1
+
+                l += 1
+
+            r += 1
+
+        return s[min_left:min_right] if min_len != len(s) + 1 else ""
